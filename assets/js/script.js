907946d114944cf42f4d3f69ae7dd67d0a2d7ded
Tmeng <tonymengt@gmail.com>
2022-08-12 18:43:41 -0400

    added first fetch call based on category selected from the drop down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,55 @@
-var testEl = document.querySelector(".dropdown")
-var test2El = document.querySelector(".test")
+var drinkCategoryEl = document.querySelector(".drink-category");
+var ingredientEl = document.querySelector(".dropdown-content");
+
+// var test2El = document.querySelector(".test")
 
 // JS to toggle the dropdown menu based on click
-testEl.addEventListener("click", function(e) {
-    e.stopPropagation()
-    testEl.classList.toggle('is-active')
-})
+var drinkCategory = function (event) {
+    event.stopPropagation();
+    drinkCategoryEl.classList.toggle('is-active');
+}
 
-test2El.addEventListener("click", function(e) {
-    e.stopPropagation()
-    test2El.classList.toggle('is-active')
-})
+// replace content within the dropdown based on what's been selected
+var selectIngredient = function (event) {
+    var buttonContent = document.querySelector(".button-content")
+    var ingredient = event.target.textContent.trim()
+    buttonContent.textContent=ingredient
+    console.log(ingredient)
+    searchDrinkIngredient(buttonContent.textContent)
+}
+
+var searchDrinkIngredient = function (buttonContent) {
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + buttonContent
+    fetch (apiUrl)
+        .then(function(response){
+            if (!response.ok) {
+                console.log("Failed to call API");
+            }
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+            searchDrinkId(data);
+        })
+        .catch(function(error){
+            console.log("error message")
+        });
+};
+
+var searchDrinkId = function (data) {
+    
+}
+
+
+
+
+drinkCategoryEl.addEventListener("click", drinkCategory);
+ingredientEl.addEventListener("click", selectIngredient);
+
+
+
+// test2El.addEventListener("click", function(e) {
+//     e.stopPropagation()
+//     test2El.classList.toggle('is-active')
+// })
 
