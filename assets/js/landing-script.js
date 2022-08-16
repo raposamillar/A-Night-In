7b3edee8b8b2fcc3a340ea096efb6f3a68d505ab
KeeveRW11 <keevewhyte+github@gmail.com>
2022-08-15 22:01:41 -0400

    slideshow working to add timer next

--- assets/js/landing-script.js
+++ assets/js/landing-script.js
@@ -1,18 +1,18 @@
-/*var buttons = document.querySelector("[data-carousel-button]")
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        var offset = button.dataset.carouselButton === "next" ? 1 : -1
-        var slides = button
-            .closest("[data-carousel]")
-            .querySelector("[data-slides]")
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
-        var activeSlide = slides.querySelector("[data-active]") 
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset  
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
-});
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
